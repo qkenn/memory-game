8ffad7b36a41a6642357e0226607ac4d759abfac
qkenn <github.com.sampling990@passinbox.com>
2024-08-22 16:56:54 +0530

    update card styling

--- src/App.jsx
+++ src/App.jsx
@@ -14,14 +14,18 @@ function App() {
     // set loadning and error to initial values
     setLoading(true);
     setErr(null);
+    setInitialData(null);
 
     const controller = new AbortController();
 
     async function fetchData() {
       try {
-        const res = await fetch('https://rickandmortyapi.com/api/character', {
-          signal: controller.signal,
-        });
+        const res = await fetch(
+          'https://rickandmortyapi.com/api/character/?page=2',
+          {
+            signal: controller.signal,
+          }
+        );
 
         // manualy handle errors
         if (res.status != 200) {
@@ -54,16 +58,17 @@ function App() {
   function generateCards(data) {
     const cards = [];
 
-    data.forEach((entry) => {
+    data.forEach((character) => {
       const clone = {};
 
       // get selected properties and values
-      clone.id = entry.id;
-      clone.name = entry.name;
-      clone.status = entry.status;
-      clone.species = entry.species;
-      clone.location = entry.location.name;
-      clone.image = entry.image;
+      clone.id = character.id;
+      clone.name = character.name;
+      clone.status = character.status;
+      clone.type = character.type;
+      clone.species = character.species;
+      clone.location = character.location.name;
+      clone.image = character.image;
 
       cards.push(clone);
     });
@@ -79,6 +84,10 @@ function App() {
     }
     return arrCopy;
   }
+
+  // function geneRandomInt(min = 1, max = 40) {
+  //   return Math.floor(Math.random() * (max - min + 1)) + min;
+  // }
 
   function handleClick() {
     const shuffledArr = shuffle([...initialData]);
@@ -96,24 +105,49 @@ function App() {
         {err && <Err message={err} />}
 
         {initialData && (
-          <ul className="grid auto-rows-[minmax(10rem,_auto)] grid-cols-[repeat(auto-fit,_minmax(30rem,_1fr))] gap-12 px-5">
+          <ul className="grid auto-rows-[minmax(10rem,_auto)] grid-cols-[repeat(auto-fit,_minmax(16rem,_1fr))] gap-12 px-5">
             {initialData.map((el) => {
+              let statusColor;
+
+              // reason
+              // tailwind does not allow dynamic classes
+              switch (el.status) {
+                case 'unknown':
+                  statusColor = 'bg-slate-600';
+                  break;
+                case 'Alive':
+                  statusColor = 'bg-blue-600';
+                  break;
+                case 'Dead':
+                  statusColor = 'bg-red-600';
+                  break;
+                default:
+                  statusColor = 'bg-slate-600';
+              }
+
               return (
                 <li
-                  key={el.name}
-                  className="flex gap-5 overflow-hidden rounded-xl bg-neutral-800 text-white"
+                  key={el.id}
+                  className="flex flex-col overflow-hidden rounded-xl bg-neutral-800 text-white"
                   onClick={handleClick}
                 >
                   <div>
-                    <img src={el.image} />
+                    <img
+                      src={el.image}
+                      className="h-full w-full object-cover"
+                    />
                   </div>
 
-                  <div className="p-4">
-                    <h3 className="text-2xl font-medium">{el.name}</h3>
-                    <p>
-                      {el.status} - {el.species}
+                  <div className="p-4 text-center">
+                    <h3 className="text-xl font-medium">{el.name}</h3>
+                    <p className="mt-1 flex items-center justify-center gap-2 text-sm">
+                      <span
+                        className={`inline-block h-2 w-2 rounded-full ${statusColor}`}
+                      ></span>
+                      <span className="text-slate-300">
+                        {el.status} - {el.species} {el.type && `(${el.type})`}
+                      </span>
                     </p>
-                    <p>{el.location}</p>
                   </div>
                 </li>
               );
