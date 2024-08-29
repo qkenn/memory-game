11971b9580648d70de8207f7c506c81e15ed43bc
qkenn <github.com.sampling990@passinbox.com>
2024-08-29 10:23:40 +0530

    create new score container fixed to the browser window

--- src/App.jsx
+++ src/App.jsx
@@ -107,11 +107,7 @@ function App() {
 
   return (
     <>
-      <Header
-        scores={{ score: gameData.score }}
-        gameOver={gameOver}
-        replayHandler={replay}
-      />
+      <Header gameOver={gameOver} replayHandler={replay} />
 
       <main className="mx-auto my-10 max-w-[90rem]">
         {loading && <Spinner />}
@@ -119,53 +115,59 @@ function App() {
         {err && <Err message={err} />}
 
         {fetchedData && (
-          <ul className="grid auto-rows-[minmax(10rem,_auto)] grid-cols-[repeat(auto-fit,_minmax(16rem,_1fr))] gap-20 px-5">
-            {fetchedData.map((el) => {
-              let statusColor;
+          <>
+            <ul className="grid auto-rows-[minmax(10rem,_auto)] grid-cols-[repeat(auto-fit,_minmax(16rem,_1fr))] gap-20 px-5">
+              {fetchedData.map((el) => {
+                let statusColor;
 
-              // tailwind does not allow dynamic classes
-              switch (el.status) {
-                case 'unknown':
-                  statusColor = 'bg-slate-600';
-                  break;
-                case 'Alive':
-                  statusColor = 'bg-blue-600';
-                  break;
-                case 'Dead':
-                  statusColor = 'bg-red-600';
-                  break;
-                default:
-                  statusColor = 'bg-slate-600';
-              }
+                // tailwind does not allow dynamic classes
+                switch (el.status) {
+                  case 'unknown':
+                    statusColor = 'bg-slate-600';
+                    break;
+                  case 'Alive':
+                    statusColor = 'bg-blue-600';
+                    break;
+                  case 'Dead':
+                    statusColor = 'bg-red-600';
+                    break;
+                  default:
+                    statusColor = 'bg-slate-600';
+                }
 
-              return (
-                <li
-                  key={el.id}
-                  className="flex flex-col overflow-hidden rounded-xl bg-neutral-800 text-white"
-                  onClick={() => playRound(el.id)}
-                >
-                  <div>
-                    <img
-                      src={el.image}
-                      className="h-full w-full object-cover"
-                    />
-                  </div>
+                return (
+                  <li
+                    key={el.id}
+                    className="flex flex-col overflow-hidden rounded-xl bg-neutral-800 text-white"
+                    onClick={() => playRound(el.id)}
+                  >
+                    <div>
+                      <img
+                        src={el.image}
+                        className="h-full w-full object-cover"
+                      />
+                    </div>
 
-                  <div className="p-4 text-center">
-                    <h3 className="text-xl font-medium">{el.name}</h3>
-                    <p className="mt-1 flex items-center justify-center gap-2 text-sm">
-                      <span
-                        className={`inline-block h-2 w-2 rounded-full ${statusColor}`}
-                      ></span>
-                      <span className="text-slate-300">
-                        {el.status} - {el.species} {el.type && `(${el.type})`}
-                      </span>
-                    </p>
-                  </div>
-                </li>
-              );
-            })}
-          </ul>
+                    <div className="p-4 text-center">
+                      <h3 className="text-xl font-medium">{el.name}</h3>
+                      <p className="mt-1 flex items-center justify-center gap-2 text-sm">
+                        <span
+                          className={`inline-block h-2 w-2 rounded-full ${statusColor}`}
+                        ></span>
+                        <span className="text-slate-300">
+                          {el.status} - {el.species} {el.type && `(${el.type})`}
+                        </span>
+                      </p>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
+
+            <div className="fixed bottom-10 left-1/2 -translate-x-1/2 rounded-lg bg-white px-7 py-3 text-xl">
+              Score: {gameData.score}
+            </div>
+          </>
         )}
       </main>
     </>
